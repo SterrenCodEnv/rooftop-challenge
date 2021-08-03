bf4b340c0b5a838b0623e808881fe103df30e8fb
SterrenCodEnv <codenv.sterren@gmail.com>
2021-08-02 22:22:20 -0300

    .gitignore

--- src/controllers/coupon.controller.ts
+++ src/controllers/coupon.controller.ts
@@ -34,14 +34,51 @@ export const getCouponByEmail = async (req: Request, res: Response) => {
 };
 
 export const createCoupon = async (req: Request, res: Response) => {
-  const newCoupon = await getRepository(Coupon).create({code: req.body.code, expiresAt: req.body.expires_at});
-  await getRepository(Coupon).save(newCoupon).then((data) => {
-      res.status(200).json({
+  const newCoupon = await getRepository(Coupon).create({
+    code: req.body.code,
+    expiresAt: req.body.expires_at,
+  });
+  await getRepository(Coupon)
+    .save(newCoupon)
+    .then((data) => {
+      res.status(201).json({
         data: data,
       });
-    }).catch((err) => {
-      res.status(404).json({
+    })
+    .catch((err) => {
+      res.status(422).json({
         message: err,
       });
     });
+};
+
+export const updateCoupon = async (req: Request, res: Response) => {
+  const { email } = req.body;
+  const id = req.params.id;
+  const existAssignedEmail = await getRepository(Coupon).find({
+    where: { customerEmail: email },
+  });
+
+  if (existAssignedEmail.length > 0) {
+    res.status(422).json({
+        message: 'Email already assigned in a coupon',
+      });
+  } 
+  const coupon = await getRepository(Coupon).findOne(id);
+  if(coupon){
+    let updateCoupon: Coupon = coupon;
+    updateCoupon.customerEmail = email;
+    updateCoupon.assignedAt = new Date();
+    getRepository(Coupon).merge(coupon, updateCoupon);
+    await getRepository(Coupon).save(coupon).then((data) => {
+      res.status(201).json({
+        data: data,
+      });
+    })
+    .catch((err) => {
+      res.status(422).json({
+        message: err,
+      });
+    });
+  }
 };
--- src/routes/coupon.routes.ts
+++ src/routes/coupon.routes.ts
@@ -1,23 +1,34 @@
 import { Router } from 'express';
-import { check } from 'express-validator';
+import { check, body } from 'express-validator';
 import { fieldsValidators } from '../middlewares/fields-validator';
 const router = Router();
 
 // Import Controllers
-import { getCoupons, getCouponByEmail, createCoupon } from '../controllers/coupon.controller'; 
+import { getCoupons, getCouponByEmail, createCoupon, updateCoupon } from '../controllers/coupon.controller'; 
 
 router.get('/coupons', getCoupons);
 
 router.get('/coupon', [
-    check('email', 'Email is required').isEmail(),
+    check('email', 'Email is required').not().isEmpty(),
+    check('email', 'Email is not valid').isEmail(),
     check('code', 'Coupon code is required').not().isEmpty(),
     check('code', 'Coupon code must have 8 letters').isLength({max: 8, min:8}),
     fieldsValidators
 ], getCouponByEmail);
 
 router.post('/coupon', [
-    check('code', 'Coupon code is required').not().isEmpty(),
-    check('code', 'Coupon code must have 8 letters').isLength({max: 8, min:8}),
+    body('code', 'Coupon code is required').not().isEmpty(),
+    body('code', 'Coupon code must have 8 letters').isLength({max: 8, min:8}),
+    body('expires_at', 'Expires at is required').not().isEmpty(),
+    fieldsValidators
 ], createCoupon);
+
+router.patch('/coupon/:id', [
+    check('id', 'Coupon Id is required').not().isEmpty(),
+    check('id', 'Coupon Id is not valid').isInt(),
+    body('email', 'Email is required').not().isEmpty(),
+    body('email', 'Email is not valid').isEmail(),
+    fieldsValidators
+], updateCoupon);
 
 export default router;
